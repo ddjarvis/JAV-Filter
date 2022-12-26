294eceef306eaedc284d80e2675e3c09d97bd5a7
David Jarvis <ddjarvis@up.edu.ph>
2022-12-26 18:51:20 +0800

    Update filter2.js

--- filter2.js
+++ filter2.js
@@ -1,6 +1,46 @@
 var list = `\
 // -=-=-=-=-= Start List =-=-=-=-=-
 
+[RCT-502] The day Stuffed Toys Suddenly Attack Beautiful Girls - Plush Rape ((Hayase Arisu; Matsushita Hikari; Nakano Arisa)) [[EngSub]].mp4
+[MEYD-576] The Neighbor's Busty Married Woman Entered My apartment by mistake, and my air Conditioner Was Broken. Misidentifying me as her Husband ((Sayama Ai)) [[EngSub]].mp4
+[MIAA-207] My Friend Akari Was My First Love, And When She Got Hired As A Maid By My Family, My Dad used Her Big Ass To Service Himself And I Couldn't Do Anything About It ((Neo Akari)) [[EngSub]].mp4
+[MIMK-078] Booking at a 1,000 Yen Barber Shop ((Yoshine Yuria)) [[EngSub]].mp4
+[HBAD-260] A Fickle Widow Gets Wet From Being Shamed And Tries Not to Wail When Getting Her Thick Lower-Half Screwed Saya Niyama ((Niiyama Saya)) [[EngSub]].mp4
+[VEC-272] We're Not Stopping Even If We Get Caught! Adultery Sex That Gets Even Hotter When The Wife Finds Out ((Kazama Yumi)) [[EngSub]].mp4
+[PRED-149] A Sweaty Investigator On The 7th Day Of A Stakeout No, What Am I Doing I'm On Duty... Midsummer Edition Toka Rinne ((Rinne Touka)) [[EngSub]].mp4
+[SNIS-058] Sweat Caregiver Can't Say No ((Okuda Saki)) [[EngSub]].mp4
+[IPX-453] A Private Tutor With No Bra Accidentally Seduces Her Student ((Momonogi Kana)) [[EngSub]].mp4
+[URE-043] Masterpiece Revival! Comic Series My Stepfather! ((Mori Hotaru)) [[EngSub]].mp4
+[WANZ-436] Orgasm Enthusiast Masao and a Horny Younger Stepsisters Sexy Fun Time Home Alone 4 ((Yoshikawa Aimi)) [[EngSub]].mp4
+[MIMK-085] Having Sex With My Super Capable Married Female Coworker On Our Business Trip! She's Always Really Serious And Scary But This Time She's Just A Woman - JULIA ((Julia)) [[EngSub]].mp4
+[DVDES-880] My Naughty Classmate's Gunning For My Busty MILF 6 I Can't Believe My Mom's Cheating And Wants to Get pregnant ((Saegusa Chitose)) [[EngSub]].mp4
+[STARS-174] This Idol Will Relentlessly Give You Tongue-On-Tongue Action, In Drooling, Slobbering Rich And Thick Kisses ((Nagano Ichika)) [[EngSub]].mp4
+[SSIS-535] My girlfriend's older sister is perfect when she takes her clothes, I fucked her from morning till night.. ((Yamate Ria)) [[EngSub]].mp4
+[PPPD-695] I Turned My Friend's Education Mama Into My Titty Sex S---e JULIA - These DQN Bad Boys Turned Her Into Their Very Own Creampie Titty Cum Bucket ((Julia)) [[EngSub]].mp4
+[MIAA-405] The Married Woman Next Door Lodges A Customer Complaint, But The Tables Are Turned When The Salesman Spots Evidence Of Her Affair - Made To Take A Creampie ((Fukada Eimi)) [[EngSub]].mp4
+[SHKD-738] Fucked A Negotiator Woman - Saki Kozai ((Kouzai Saki)) [[EngSub]].mp4
+[MEYD-336] I Wish I Never Found Out That My Husband's Son Had Such A Huge Cock.. ((Honda Misaki)) [[EngSub]].mp4
+[SSNI-964] I'm So Sorry Darling Cheating Wife Has Secret Trysts With The Landlord To Save Her Family's Home ((Aoi Tsukasa)) [[EngSub]].mp4
+[URE-065] Hayoshinema - The Girl Everbody's Talking About ((Kazama Yumi)) [[EngSub]].mp4
+[URE-074] I Shouldn't Have bought Doujinshi Without Telling My Wife - Original Story - Minamoto - Ryo Ayumi ((Hayashi Ryou)) [[EngSub]].mp4
+[URE-068] Oops, I Cheated - Pure, Chaste-Seeming Married Woman Brimming With Secret Lust - Original By Zenzen Yamamoto ((Hatsune Minori)) [[EngSub]].mp4
+[URE-066] The Slutty Wives Of The Shopping District - Original Work - Fetakishi ((Mizuno Asahi)) [[EngSub]].mp4
+[MIMK-071] Cuckold Daydream Syndrome ((Kimijima Mio)) [[EngSub]].mp4
+[DASD-683] I Have Strong Cuckold Desires, And I Started Having Daydream Fantasies About Watching My Wife Get Fucked By Other Men ((Kimijima Mio)) [[EngSub]].mp4
+[ADN-162] A Cock Assault This Married Woman Housemaid Is Getting R-ped Against Her Will ((Matsushita Saeko)) [[EngSub]].mp4
+[SDMF-017] I am an Older woman who takes care of my son sexual desire. Infinite Creampies 8 - Peach-colored Family VOL.19 ((Hiraoka Rieko)) [[EngSub]].mp4
+[PRED-208] Class Reunion NTR - My Wife Infidelity Creampie Video with Her Bastard Ex-Boyfriend - JULIA ((Julia)) [[EngSub]].mp4
+[SHKD-512] Fucked In Front Of Her Husband - Target III (Misaki Honda ((Honda Misaki)) [[EngSub]].mp4
+[PPPD-519] What If It Was OK To R-pe And Creampie Big Breasted Girls.. ((Hitomi)) [[EngSub]].mp4
+[YMDD-177] Melody Hina Marks - Japanese Hospitality - Young Hostess Edition ((Melody Hiina Marks)) [[EngSub]].mp4
+[SSNI-850] We Discovered This Skinny Girl, But Now She's Become A Voluptuous Beauty A CG Collection Of FANZA And Fan Fiction That Became A Hit And Achieved Record-Breaking Downloading Numbers A Live-Action M.mp4
+[MIGD-771] My Little Sister Is Getting Creampie Doggy Styled And Begging Me For Help - Yuna Himekawa ((Himekawa Yuuna)) [[EngSub]].mp4
+[MIAA-122] My High School Crush Hides Me Under Her Bed While She Gets Fucked By A Guy I Hate! I Feel Shocked And Turned On At The Same Time.. ((Fukada Eimi)) [[EngSub]].mp4
+[URE-049] Original Story By Ko Takasugi , The Master Of Fakecest Porn. Live-Action Adaptation Of The Story Of Beautiful Mothers Addicted To Immoral Fakecestuous Sex! ((Kimijima Mio; Shiraki Yuuko)) [[EngSub]].mp4
+[RBD-902] New Sl-ve Police Inspector 4 ((Mochida Akane; Shiina Sora; Shinoda Yuu)) [[EngSub]].mp4
+[MIAA-146] I Saw My Lil Sister getting r-ped By Mom's New Husband And I Got a boner! ((Nagase Yui)) [[EngSub]].mp4
+[PRED-159] Dirty Step Father Brainwashes His Step Daughters We're Happy To Be Our New Father's Sex Toys. Eimi Fukuda ((Fukada Eimi; Hoshina Ai)) [[EngSub]].mp4
+[ABP-950] Extreme Situations #18 - Perfect POV! - Presents 4 Super Sexy Situations ((Suzumori Remu)) [[EngSub]].mp4
 
 
 // -=-=-=-=-=  End List  =-=-=-=-=-
